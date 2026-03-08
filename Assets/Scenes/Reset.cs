b42e422923a8003a34dd21c4e10159c89ee3327a
DS Dude <wwwizardking64@gmail.com>
2026-03-08 19:10:45 -0400

    Added Reset Functionality

---
+++ Assets/Scenes/Reset.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetScene();
+        }
+
+    }
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
