dc2d2e46e9f5455b9ee668e7c5aa270dc412149f
Arman Khademhir <armankhadem6@gmail.com>
2026-03-17 03:31:03 -0400

    Bugs
    There were alot of bugs with the script so i removed it, also have fixed the rock texture and moved the ui elements, as well as making the ui be changing based on the size of the resolution

--- Assets/Scripts/Arman/SummonThrower.cs
+++ Assets/Scripts/Arman/SummonThrower.cs
@@ -1,162 +1,66 @@
 using UnityEngine;
 using TMPro;
-using System.Collections.Generic;
 
 public class SummonThrower : MonoBehaviour
 {
-    [Header("Throwing")]
     public GameObject[] throwableObjects;
-    public Sprite[] objectIcons;
     public Transform throwPoint;
     public float throwForce = 30f;
 
-    [Header("UI")]
     public TextMeshProUGUI selectedObjectText;
-    public GameObject selectionPanel;
-    public Transform buttonContainer;
-    public GameObject objectButtonPrefab;
 
-    private int currentObject = 0;
-    private GameObject heldObject;
-    private Rigidbody heldRb;
-    private bool menuOpen = false;
+    int currentObject = 0;
 
     void Start()
     {
         UpdateSelectedText();
-        SpawnHeldObject();
-        BuildObjectMenu();
-
-        if (selectionPanel != null)
-            selectionPanel.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        // Press 1 -> previous object
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            ToggleMenu();
+            currentObject--;
+            if (currentObject < 0)
+                currentObject = throwableObjects.Length - 1;
+
+            UpdateSelectedText();
         }
 
-        if (menuOpen)
-            return;
+        // Press 2 -> next object
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentObject++;
+            if (currentObject >= throwableObjects.Length)
+                currentObject = 0;
 
+            UpdateSelectedText();
+        }
+
+        // Throw object
         if (Input.GetKeyDown(KeyCode.F))
         {
             ThrowObject();
         }
     }
 
-    void ToggleMenu()
-    {
-        menuOpen = !menuOpen;
-
-        if (selectionPanel != null)
-            selectionPanel.SetActive(menuOpen);
-
-        Cursor.lockState = menuOpen ? CursorLockMode.None : CursorLockMode.Locked;
-        Cursor.visible = menuOpen;
-    }
-
-    void BuildObjectMenu()
-    {
-        if (buttonContainer == null || objectButtonPrefab == null) return;
-
-        foreach (Transform child in buttonContainer)
-        {
-            Destroy(child.gameObject);
-        }
-
-        for (int i = 0; i < throwableObjects.Length; i++)
-        {
-            GameObject buttonObj = Instantiate(objectButtonPrefab, buttonContainer);
-
-            ObjectSlotUI slot = buttonObj.GetComponent<ObjectSlotUI>();
-            if (slot != null)
-            {
-                Sprite icon = null;
-                if (objectIcons != null && i < objectIcons.Length)
-                    icon = objectIcons[i];
-
-                slot.Setup(i, throwableObjects[i].name, icon, this);
-            }
-        }
-    }
-
-    public void SelectObject(int index)
-    {
-        if (index < 0 || index >= throwableObjects.Length) return;
-
-        currentObject = index;
-        UpdateSelectedText();
-        SpawnHeldObject();
-
-        menuOpen = false;
-
-        if (selectionPanel != null)
-            selectionPanel.SetActive(false);
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    void SpawnHeldObject()
+    void ThrowObject()
     {
         if (throwableObjects.Length == 0) return;
 
-        if (heldObject != null)
-        {
-            Destroy(heldObject);
-        }
-
-        heldObject = Instantiate(
+        GameObject obj = Instantiate(
             throwableObjects[currentObject],
             throwPoint.position,
-            throwPoint.rotation,
-            throwPoint
+            throwPoint.rotation
         );
 
-        heldObject.transform.localPosition = Vector3.zero;
-        heldObject.transform.localRotation = Quaternion.identity;
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
 
-        heldRb = heldObject.GetComponent<Rigidbody>();
-
-        if (heldRb != null)
+        if (rb != null)
         {
-            heldRb.isKinematic = true;
-            heldRb.useGravity = false;
+            rb.AddForce(throwPoint.forward * throwForce, ForceMode.Impulse);
         }
-
-        Collider[] cols = heldObject.GetComponentsInChildren<Collider>();
-        foreach (Collider col in cols)
-        {
-            col.enabled = false;
-        }
-    }
-
-    void ThrowObject()
-    {
-        if (heldObject == null) return;
-
-        heldObject.transform.parent = null;
-
-        if (heldRb != null)
-        {
-            heldRb.isKinematic = false;
-            heldRb.useGravity = true;
-            heldRb.AddForce(throwPoint.forward * throwForce, ForceMode.Impulse);
-        }
-
-        Collider[] cols = heldObject.GetComponentsInChildren<Collider>();
-        foreach (Collider col in cols)
-        {
-            col.enabled = true;
-        }
-
-        heldObject = null;
-        heldRb = null;
-
-        SpawnHeldObject();
     }
 
     void UpdateSelectedText()
